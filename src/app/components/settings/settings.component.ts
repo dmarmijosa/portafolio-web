e6a455378f8dca5fec3724730f657b1a9c2f1639
Danny Armijos <daniel2667@hotmail.es>
2023-06-08 23:28:30 +0200

    cambios y  mejoras

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,26 +1,36 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SettingsComponent {
-  
   currentLanguage: string = 'es';
-  constructor(private translateService:TranslateService){
-
-  }
-  changeLanguage(){
+  constructor(
+    private translateService: TranslateService,
+    private dataService: DataService
+  ) {}
+  changeLanguage() {
     if (this.currentLanguage === 'es') {
-      
       this.translateService.use('en');
       this.currentLanguage = 'en';
+      this.dataService.setData(this.currentLanguage);
     } else {
       this.translateService.use('es');
       this.currentLanguage = 'es';
+      this.dataService.setData(this.currentLanguage);
+    }
+  }
+
+
+  descargar() {
+    if (this.currentLanguage === 'en') {
+      window.open('./assets/pdfs/cv/cv-en.pdf', '_blank');
+    } else {
+      window.open('./assets/pdfs/cv/cv-es.pdf', '_blank');
     }
   }
 }
--- src/app/pages/about/profile/profile.component.ts
+++ src/app/pages/about/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,11 +8,24 @@ import { TranslateService } from '@ngx-translate/core';
   styles: [],
 })
 export class ProfileComponent {
+  data: any;
   year: Date = new Date();
   nacimiento = this.year.getFullYear() - 1992;
-  constructor(private translate: TranslateService) {
-    const currentLang = this.translate.currentLang;
-    console.log('El idioma actual es:', translate);
+  constructor(private dataService: DataService) {
+    this.dataService.data$.subscribe(data => {
+      this.data = data;
+
+    });
+
+
+  }
+  descargar(){
+    if(this.data==='en'){
+      window.open('./assets/pdfs/cv/cv-en.pdf', '_blank');
+    }else{
+      window.open('./assets/pdfs/cv/cv-es.pdf', '_blank');
+      
+    }
   }
 
   
